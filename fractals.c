090d26f2da17198c8c7444c864465f2195f79dc3
renaud <renaud.bd@gmail.com>
2024-09-29 00:36:30 +0200

    first commit

---
+++ fractals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+
+int main(int argc, char *argv[]){
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WIND  OWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
