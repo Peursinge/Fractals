42bccd6d8da7a2ee50116837fa62036d0652b4a5
Reno <renaud.bd@gmail.com>
2024-09-29 15:24:48 +0200

    modified git ignore

--- fractals.c
+++ fractals.c
@@ -4,10 +4,9 @@
 
 int main(int argc, char *argv[]){
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WIND  OWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
+    SDL_Window *window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
     SDL_Delay(2000);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    //aaaaaaaaaaaaaaaa
     return 0;
 }
