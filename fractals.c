b6e113602dec876fc9a21d62fed0c1431b081627
Reno <renaud.bd@gmail.com>
2024-10-09 14:43:49 +0200

    dot fct

--- fractals.c
+++ fractals.c
@@ -3,10 +3,21 @@
 #include <SDL2/SDL.h>
 
 int main(int argc, char *argv[]){
+    SDL_Renderer* renderer = NULL;
+    SDL_Window *window = NULL;
+
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_OPENGL);
-    SDL_Delay(2000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawPoint(renderer, 640/2, 480/2);
+
+    SDL_RenderPresent(renderer);
+    SDL_delay(10000);
+
+    
     return 0;
 }
